740cdbf8f976bd9b89ef5e071a1e19ae24958bf1
Tim Brennan <t.brennan874@gmail.com>
2023-09-05 20:57:10 -0400

    implemented a search functionality

--- types/index.ts
+++ types/index.ts
@@ -6,3 +6,7 @@ export interface CustomButtonProps {
   handleClick?: MouseEventHandler<HTMLButtonElement>;
   btnType?: "button" | "submit";
 }
+export interface searchManufacturerProps {
+  manufacturer: string;
+  setManufacturer: (manufacturer: string) => void;
+}
